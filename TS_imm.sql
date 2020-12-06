46fb1b578e377f7a73bc13263ee870146dd65ab2
Guillaume Cleroux <73357644+gcleroux@users.noreply.github.com>
2020-12-05 21:26:32 -0500

    Delete TS_imm.sql

--- TS_imm.sql
+++
@@ -1,5 +0,0 @@
-/*############################################################################
- un script SQL pour les interfaces MM offertes – vues, routines et déclencheurs (triggers)
- ############################################################################*/
-
- -- I'm gonna be honest, I have no fucking clue what this is supposed to be
